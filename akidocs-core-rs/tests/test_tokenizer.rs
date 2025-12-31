6a6bb7488d9d8427cd3b5a056e4f1a0155974510
AkiPoh <59505647+AkiPoh@users.noreply.github.com>
2025-12-31 16:14:02 +0200

    Add passing header tests

--- akidocs-core-rs/tests/test_tokenizer.rs
+++ akidocs-core-rs/tests/test_tokenizer.rs
@@ -105,3 +105,107 @@ fn test_header_level_one() {
         }),]
     );
 }
+
+#[test]
+fn test_header_levels() {
+    assert_eq!(
+        tokenize("# One")[0],
+        Token::Header(Header {
+            level: 1,
+            content: String::from("One")
+        })
+    );
+    assert_eq!(
+        tokenize("## Two")[0],
+        Token::Header(Header {
+            level: 2,
+            content: String::from("Two")
+        })
+    );
+    assert_eq!(
+        tokenize("### Three")[0],
+        Token::Header(Header {
+            level: 3,
+            content: String::from("Three")
+        })
+    );
+    assert_eq!(
+        tokenize("#### Four")[0],
+        Token::Header(Header {
+            level: 4,
+            content: String::from("Four")
+        })
+    );
+    assert_eq!(
+        tokenize("##### Five")[0],
+        Token::Header(Header {
+            level: 5,
+            content: String::from("Five")
+        })
+    );
+    assert_eq!(
+        tokenize("###### Six")[0],
+        Token::Header(Header {
+            level: 6,
+            content: String::from("Six")
+        })
+    );
+}
+
+#[test]
+fn test_header_requires_space() {
+    let result = tokenize("#NoSpace");
+    assert_eq!(
+        result,
+        vec![Token::Paragraph(Paragraph {
+            content: String::from("#NoSpace")
+        }),]
+    );
+}
+
+#[test]
+fn test_header_empty_content() {
+    let result = tokenize("##");
+    assert_eq!(
+        result,
+        vec![Token::Header(Header {
+            level: 2,
+            content: String::from("")
+        }),]
+    );
+}
+
+#[test]
+fn test_header_level_seven_becomes_paragraph() {
+    let result = tokenize("####### Seven hashes");
+    assert_eq!(
+        result,
+        vec![Token::Paragraph(Paragraph {
+            content: String::from("####### Seven hashes")
+        }),]
+    );
+}
+
+#[test]
+fn test_header_with_tab_separator() {
+    let result = tokenize("#\tHello");
+    assert_eq!(
+        result,
+        vec![Token::Header(Header {
+            level: 1,
+            content: String::from("Hello")
+        }),]
+    );
+}
+
+#[test]
+fn test_header_with_mixed_whitespace_separator() {
+    let result = tokenize("##   \t\t   \t\t   Mixed");
+    assert_eq!(
+        result,
+        vec![Token::Header(Header {
+            level: 2,
+            content: String::from("Mixed")
+        }),]
+    );
+}
